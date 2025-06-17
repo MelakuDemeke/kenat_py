2964f2c07f2f043debf8b99e2d6072a5a8191cc7
MelakuDemeke <melaku.demeke789@gmail.com>
2025-06-17 13:19:40 +0300

    Add add_months function to calculate new Ethiopian date after adding specified months

--- kenat/day_arithmetic.py
+++ kenat/day_arithmetic.py
@@ -33,3 +33,31 @@ def add_days(ethiopian, days):
             
     return {'year': year, 'month': month, 'day': day}
 
+def add_months(ethiopian, months):
+    """
+    Adds a specified number of months to an Ethiopian date.
+
+    Args:
+        ethiopian (dict): The starting Ethiopian date {'year', 'month', 'day'}.
+        months (int): The number of months to add.
+
+    Returns:
+        dict: The resulting Ethiopian date.
+    """
+    validate_ethiopian_date_object(ethiopian, 'add_months', 'ethiopian') # 
+    validate_numeric_inputs('add_months', months=months) # 
+    
+    year, month, day = ethiopian['year'], ethiopian['month'], ethiopian['day']
+    
+    total_months = month + months # 
+    year += (total_months - 1) // 13 # 
+    month = ((total_months - 1) % 13) + 1 # 
+
+    # If the original day is greater than the number of days in the new month,
+    # cap it at the last day of the new month.
+    days_in_target_month = get_ethiopian_days_in_month(year, month) # 
+    if day > days_in_target_month: # 
+        day = days_in_target_month # 
+        
+    return {'year': year, 'month': month, 'day': day}
+
