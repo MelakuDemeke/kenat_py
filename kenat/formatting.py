07abb67979d13fbf9a375ffb0129b4bb3d95e967
MelakuDemeke <melaku.demeke789@gmail.com>
2025-06-17 13:58:38 +0300

    Add format_standard function to format Ethiopian dates with language-specific month names

---
+++ kenat/formatting.py
@@ -0,0 +1,13 @@
+from .geez_converter import to_geez
+from .constants import MONTH_NAMES, DAYS_OF_WEEK
+from .utils import get_weekday
+
+def format_standard(et_date, lang='amharic'):
+    """
+    Formats an Ethiopian date using a language-specific month name and Arabic numerals.
+    Example: "መስከረም 10 2016" 
+    """
+    names = MONTH_NAMES.get(lang, MONTH_NAMES['amharic']) 
+    month_name = names[et_date['month'] - 1] 
+    return f"{month_name} {et_date['day']} {et_date['year']}" 
+
