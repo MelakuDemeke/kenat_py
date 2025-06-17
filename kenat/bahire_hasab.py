01fbc4158a4d12a2ab7ec9771871d41245a5a8a5
MelakuDemeke <melaku.demeke789@gmail.com>
2025-06-17 13:35:27 +0300

    Add get_bahire_hasab function to calculate Bahire Hasab values for a given Ethiopian year

--- kenat/bahire_hasab.py
+++ kenat/bahire_hasab.py
@@ -52,3 +52,59 @@ def _calculate_bahire_hasab_base(ethiopian_year):
         'mebaja_hamer': mebaja_hamer,  
         'nineveh_date': nineveh_date,  
     }
+
+def get_bahire_hasab(ethiopian_year, lang='amharic'):
+    """
+    Calculates all Bahire Hasab values for a given Ethiopian year. 
+
+    Args:
+        ethiopian_year (int): The Ethiopian year to calculate for.
+        lang (str): The language for names ('amharic' or 'english'). 
+
+    Returns:
+        dict: An object containing all the calculated Bahire Hasab values. 
+    """
+    validate_numeric_inputs('get_bahire_hasab', ethiopian_year=ethiopian_year)  
+    
+    base = _calculate_bahire_hasab_base(ethiopian_year)  
+
+
+    evangelist_remainder = base['amete_alem'] % 4  
+    evangelist_name = EVANGELIST_NAMES.get(lang, EVANGELIST_NAMES['english'])[evangelist_remainder]  
+
+    tinte_qemer = (base['amete_alem'] + base['metene_rabiet']) % 7  
+    weekday_index = (tinte_qemer + 1) % 7  
+    new_year_weekday = DAYS_OF_WEEK.get(lang, DAYS_OF_WEEK['english'])[weekday_index]  
+
+    movable_feasts = {}  
+    tewsak_to_key_map = {v: k for k, v in KEY_TO_TEWSAK_MAP.items()}  
+    
+    for tewsak_key, tewsak_value in MOVABLE_HOLIDAY_TEWSAK.items():  
+        holiday_key = tewsak_to_key_map.get(tewsak_key)  
+        if holiday_key:  
+            date = add_days(base['nineveh_date'], tewsak_value)  
+            info = HOLIDAY_INFO.get(holiday_key, {})  
+            rules = MOVABLE_HOLIDAYS.get(holiday_key, {})  
+            
+            movable_feasts[holiday_key] = {  
+                'key': holiday_key,  
+                'tags': rules.get('tags', []),  
+                'movable': True,  
+                'name': info.get('name', {}).get(lang) or info.get('name', {}).get('english'),  
+                'description': info.get('description', {}).get(lang) or info.get('description', {}).get('english'),  
+                'ethiopian': date,  
+                'gregorian': to_gc(date['year'], date['month'], date['day'])  
+            }
+
+    return {  
+        'ameteAlem': base['amete_alem'],
+        'evangelist': {'name': evangelist_name, 'remainder': evangelist_remainder},
+        'newYear': {'dayName': new_year_weekday, 'tinteQemer': tinte_qemer},
+        'wenber': base['wenber'],
+        'abektie': base['abektie'],
+        'metqi': base['metqi'],
+        'bealeMetqi': {'date': base['beale_metqi_date'], 'weekday': base['beale_metqi_weekday']},
+        'mebajaHamer': base['mebaja_hamer'],
+        'nineveh': base['nineveh_date'],
+        'movableFeasts': movable_feasts
+    }
